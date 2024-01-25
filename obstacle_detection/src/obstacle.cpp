9f3123bb417598e5d7d07fe3bb64981e7eaa532e
EmanueleA99 <emanuelearilli@gmail.com>
2024-01-25 10:48:27 +0100

    Obstacle sistemata, aggiunto topic booleano e parametro per la regione di interesse, finale

--- obstacle_detection/src/obstacle.cpp
+++ obstacle_detection/src/obstacle.cpp
@@ -1,7 +1,12 @@
+// Emanuele Arilli - Master's Thesis //
+// emanuelearilli@gmail.com          //
+// January 2024                      //
+
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/image.hpp"
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/opencv.hpp>
+#include "std_msgs/msg/bool.hpp"
 
 using namespace std::placeholders;
 
@@ -24,62 +29,70 @@ public:
     mDepthSub = create_subscription<sensor_msgs::msg::Image>(
       "/camera/aligned_depth_to_color/image_raw", depth_qos, std::bind(&MinimalDepthSubscriber::depthCallback, this, _1));
 
+    //Publisher for topic "rover/obstacle"
+    mObstaclePub = create_publisher<std_msgs::msg::Bool>("rover/obstacle", 10);
+
     this->declare_parameter("Obstacle_threshold", 1500.0);
+    this->declare_parameter("Region_of_interest", 500);
 
   }
 
 protected:
   void depthCallback(const sensor_msgs::msg::Image::SharedPtr msg)
   {
-    //converto il messaggio in Valore cv::mat
+    //Convert msg in cv::mat value
     cv_bridge::CvImagePtr cv_ptr;
     cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::TYPE_16UC1); //immagine di profondità
 
-    //estrapolo grandezza matrice cv::mat
+    //eextract cv::mat matrix dimensions
     int x = cv_ptr->image.cols;
     int y = cv_ptr->image.rows;
         
-    // Definisco la zona di interesse
-    int roiSize = 500;
+    // Define region of interest
+    int region = this->get_parameter("Region_of_interest").as_int();
+    int roiSize = region;
     int roiX = x / 2 - roiSize / 2;
     int roiY = y / 2 - roiSize / 2;   
 
-    // Assicuro che la zona di interesse sia completamente contenuta nell'immagine
+    // Check dimensions
     roiX = std::max(0, std::min(roiX, x - roiSize));
     roiY = std::max(0, std::min(roiY, y - roiSize));
 
-    // Estraggo la zona di interesse
+    // Extract region of interest
     cv::Mat roi = cv_ptr->image(cv::Rect(roiX, roiY, roiSize, roiSize));
     
-    // Crea una maschera per i valori non zero - prendo solo i valori con confidence
+    // Create a mask for non zero value - take only value with confidence
     cv::Mat nonZeroMask = (roi > 0);
-    // Calcolo la media dei valori di profondità nella zona di interesse
+    // Take the mean depth value 
     double meanDepth = cv::mean(roi,nonZeroMask)[0];
 
-    // Grafico il rettangolo di interesse sulla copia dell'immagine
+    // Draw a region of interest rect on the image
     cv::Mat imageWithROI = cv_ptr->image.clone();
     cv::rectangle(imageWithROI, cv::Rect(roiX, roiY, roiSize, roiSize), cv::Scalar(0, 255, 0), 2);
 
-    // Mostro l'immagine con il rettangolo di interesse
-    //cv::namedWindow(OPENCV_WINDOW);
-    //cv::imshow(OPENCV_WINDOW, imageWithROI);
-    //cv::waitKey(1);
+    // Show the image - comment this section for fast code
+    cv::namedWindow(OPENCV_WINDOW);
+    cv::imshow(OPENCV_WINDOW, imageWithROI);
+    cv::waitKey(1);
 
-    // Output the measure
-    //std::cout << "Valore di profondità al pixel (" << x << ", " << y << "): " << depthValue << std::endl;
-    // Output della media dei valori di profondità nella zona di interesse
+    // Output mean depth in the region of interest
     std::cout << "Media dei valori di profondità nella zona di interesse: " << meanDepth << std::endl;
     
-    // Verifica se la distanza media è inferiore a "param" e stampa il messaggio appropriato
+    // Check if the mean depth is less than obstacle threshold
     double obstacle_threshold = this->get_parameter("Obstacle_threshold").as_double();
 
     if (meanDepth < obstacle_threshold) {
         std::cout << "OBSTACLE DETECTED" << std::endl;
+        //publish bool msg on topic 
+        auto obstacle_msg = std_msgs::msg::Bool();
+        obstacle_msg.data = true;
+        mObstaclePub->publish(obstacle_msg);
     }
   }
 
 private:
   rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr mDepthSub;
+  rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr mObstaclePub;
 };
 
 // The main function
